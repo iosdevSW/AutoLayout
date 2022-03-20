1189a4b7ec2a5a21891c1bba438841f10bd3f456
SangWoo Shin <ssw522b@naver.com>
2022-03-21 01:49:01 +0900

    Study SizeClass(variation)

---
+++ SizeClass_rotation/SizeClass_rotation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SizeClass_rotation
+//
+//  Created by 신상우 on 2022/03/21.
+//
+
+import UIKit
+
+/* google에 hig 치면 Human Interface Guidelines 가 나옴 애플에서 제공하는 가이드라인
+ 들어가서 iOS -> Visual Design -> Adaptivity and Layout 에서 내리면 Size class 가이드라인 나옴.
+ iPhone기준  portrait  는 Regular hegiht , Compact width
+            landscape 는 Compact height
+ 
+ => 즉 height가 중요하다 width는 any로!
+ */
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
